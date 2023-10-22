143b361487afe637a2fdac598088b24149268b13
umurgdk <umurgdk@gmail.com>
2023-10-22 17:37:56 +0300

    Initial Commit

---
+++ XUILayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XUILayout
+//
+//  Created by Umur Gedik on 22.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
